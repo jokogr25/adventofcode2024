db3b10146cd143063cec93b745963f1c071cd678
jokogr (Joscha Konstantin Groth) <jokogr@mailbox.org>
2024-12-08 13:34:31 +0100

    Implement Day1.part2

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,11 @@
 module Main (main) where
 
-import qualified Day1.Day1 as Day1 (part1)
+import qualified Day1.Day1 as Day1 (part1, part2)
 
 main :: IO ()
 main = do
   distance <- Day1.part1
+  sum <- Day1.part2
   print distance
+  print sum
   return ()
--- src/Day1/Day1.hs
+++ src/Day1/Day1.hs
@@ -1,13 +1,12 @@
 {-# OPTIONS_GHC -Wno-incomplete-uni-patterns #-}
 
-module Day1.Day1 (part1) where
+module Day1.Day1 (part1, part2) where
 
 import qualified Data.List as List
 
 part1 :: IO Int
 part1 = do
-  input <- readFile "src/Day1/input.txt"
-  let (a, b) = unzip (map parseLine $ lines input)
+  (a, b) <- dayOneReadFile
 
   return $
     calcDistance
@@ -16,10 +15,21 @@ part1 = do
           (List.sort b)
       )
 
+calcDistance :: [(Int, Int)] -> Int
+calcDistance = foldl (\acc (a, b) -> acc + abs (a - b)) 0
+
+part2 :: IO Int
+part2 = do
+  (a, b) <- dayOneReadFile
+
+  return (foldl (\acc x -> acc + length (filter (== x) b) * x) 0 a)
+
+dayOneReadFile :: IO ([Int], [Int])
+dayOneReadFile = do
+  input <- readFile "src/Day1/input.txt"
+  return $ unzip (map parseLine $ lines input)
+
 parseLine :: String -> (Int, Int)
 parseLine line =
   let [x, y] = words line
    in (read x, read y)
-
-calcDistance :: [(Int, Int)] -> Int
-calcDistance = foldl (\acc (a, b) -> acc + abs (a - b)) 0
