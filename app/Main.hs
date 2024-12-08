508ac7d4680a1ab17ea870823f4ac980b0ce5b2f
jokogr (Joscha Konstantin Groth) <jokogr@mailbox.org>
2024-12-08 11:45:26 +0100

    Implement working impl for test file

--- app/Main.hs
+++ app/Main.hs
@@ -4,5 +4,6 @@ import qualified Day1.Day1 as Day1 (day1)
 
 main :: IO ()
 main = do
-  numbers <- Day1.day1
-  print numbers
+  distance <- Day1.day1
+  print distance
+  return ()
--- src/Day1/Day1.hs
+++ src/Day1/Day1.hs
@@ -1,14 +1,21 @@
 {-# OPTIONS_GHC -Wno-incomplete-uni-patterns #-}
 
-module Day1.Day1 (readInput) where
+module Day1.Day1 (day1) where
 
-readInput :: IO [(Int, Int)]
-readInput = do
-  input <- readFile "src/Day1/input.txt"
+import qualified Data.List as List
+import qualified Data.Set as Set
+
+day1 :: IO Int
+day1 = do
+  input <- readFile "src/Day1/test.txt"
   let numbers = map parseLine $ lines input
-  return numbers
+  let (a, b) = unzip numbers
+  return $ calcDistance (zip (List.sort $ Set.toList $ Set.fromList a) (List.sort $ Set.toList $ Set.fromList b))
 
 parseLine :: String -> (Int, Int)
 parseLine line =
   let [x, y] = words line
    in (read x, read y)
+
+calcDistance :: [(Int, Int)] -> Int
+calcDistance = foldl (\acc (a, b) -> acc + abs (a - b)) 0
