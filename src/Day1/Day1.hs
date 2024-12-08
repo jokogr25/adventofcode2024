50d67379c6405c0ec3bb3e21984812dffbf4552b
jokogr (Joscha Konstantin Groth) <jokogr@mailbox.org>
2024-12-08 12:09:12 +0100

    Implement part 1 by removing converting to set

--- src/Day1/Day1.hs
+++ src/Day1/Day1.hs
@@ -3,14 +3,18 @@
 module Day1.Day1 (day1) where
 
 import qualified Data.List as List
-import qualified Data.Set as Set
 
 day1 :: IO Int
 day1 = do
-  input <- readFile "src/Day1/test.txt"
-  let numbers = map parseLine $ lines input
-  let (a, b) = unzip numbers
-  return $ calcDistance (zip (List.sort $ Set.toList $ Set.fromList a) (List.sort $ Set.toList $ Set.fromList b))
+  input <- readFile "src/Day1/input.txt"
+  let (a, b) = unzip (map parseLine $ lines input)
+
+  return $
+    calcDistance
+      ( zip
+          (List.sort a)
+          (List.sort b)
+      )
 
 parseLine :: String -> (Int, Int)
 parseLine line =
