548e9b3c180ed7641f811183bc1671606c0f85f5
jokogr (Joscha Konstantin Groth) <jokogr@mailbox.org>
2024-12-08 12:12:09 +0100

    Rename

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,9 @@
 module Main (main) where
 
-import qualified Day1.Day1 as Day1 (day1)
+import qualified Day1.Day1 as Day1 (part1)
 
 main :: IO ()
 main = do
-  distance <- Day1.day1
+  distance <- Day1.part1
   print distance
   return ()
--- src/Day1/Day1.hs
+++ src/Day1/Day1.hs
@@ -1,11 +1,11 @@
 {-# OPTIONS_GHC -Wno-incomplete-uni-patterns #-}
 
-module Day1.Day1 (day1) where
+module Day1.Day1 (part1) where
 
 import qualified Data.List as List
 
-day1 :: IO Int
-day1 = do
+part1 :: IO Int
+part1 = do
   input <- readFile "src/Day1/input.txt"
   let (a, b) = unzip (map parseLine $ lines input)
 
