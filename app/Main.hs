0662b8f92efb0e0cc72d22b2a5dc18d19e93e4e7
jokogr (Joscha Konstantin Groth) <jokogr@mailbox.org>
2024-12-09 17:43:13 +0100

    Solve Day2.Part2 - Christian is great

--- app/Main.hs
+++ app/Main.hs
@@ -1,16 +1,20 @@
 module Main (main) where
 
 import qualified Day1.Day1 as Day1 (part1, part2)
-import qualified Day2.Day2 as Day2 (part1)
+import qualified Day2.Day2 as Day2 (part1, part2)
 
 main :: IO ()
 main = do
   day1part1 <- Day1.part1
-  day1part2 <- Day1.part2
   print ("Day1 - Part 1: " ++ show day1part1)
+
+  day1part2 <- Day1.part2
   print ("Day1 - Part 2: " ++ show day1part2)
 
   day2part1 <- Day2.part1
   print ("Day2 - Part 1: " ++ show day2part1)
+
+  day2part2 <- Day2.part2
+  print ("Day2 - Part 2: " ++ show day2part2)
 
   return ()
--- src/Day2/Day2.hs
+++ src/Day2/Day2.hs
@@ -1,15 +1,20 @@
 {-# OPTIONS_GHC -Wno-unused-matches #-}
 
-module Day2.Day2 (part1) where
+module Day2.Day2 (part1, part2) where
 
 data Direction = Up | Down | Init | None deriving (Show, Eq)
+
+data Direction' = Up' | Down' deriving (Show, Eq)
 
 part1 :: IO Int
 part1 = do
   input <- dayTwoReadFile
-  print input
-  print (map (isSafe Init) input)
   length . filter id . map (isSafe Init) <$> dayTwoReadFile
+
+part2 :: IO Int
+part2 = do
+  input <- dayTwoReadFile
+  length . filter id . map (isSafe2' []) <$> dayTwoReadFile
 
 isSafe :: Direction -> [Int] -> Bool
 isSafe _ [] = False
@@ -20,6 +25,11 @@ isSafe dir (x : xs : xss)
   | (abs (x - xs) >= 1 && abs (x - xs) <= 3) && checkDirection x xs == dir = isSafe (checkDirection x xs) (xs : xss)
   | otherwise = False
 
+-- (x : xs) von der zweiten Liste x ist der erste Wert von der dieser
+isSafe2' :: [Int] -> [Int] -> Bool
+isSafe2' _ [] = False
+isSafe2' x (y : ys) = isSafe2' (x ++ [y]) ys || isSafe Init (x ++ ys)
+
 checkDirection :: Int -> Int -> Direction
 checkDirection x y
   | x < y = Up
@@ -28,7 +38,7 @@ checkDirection x y
 
 dayTwoReadFile :: IO [[Int]]
 dayTwoReadFile = do
-  input <- readFile "src/Day2/input.txt"
+  input <- readFile "src/Day2/example.txt"
   return $ map parseLine (lines input)
 
 parseLine :: String -> [Int]
